067554cf9bd8800ff2945cd2839b9e53e7366984
Nithin Joji <126151645+Nithin5754@users.noreply.github.com>
2023-05-01 23:42:20 +0530

    half finished

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-const sideLinks=document.querySelectorAll('a')
+const sideLinks=document.querySelector('.sidebar').querySelectorAll('a')
 
 
 sideLinks.forEach((sideLinksEl)=>{
@@ -7,6 +7,26 @@ sideLinks.forEach((sideLinksEl)=>{
     document.querySelector('.active')?.classList.remove('active');
     sideLinksEl.classList.add('active')
   })
+})
+
+const menuBtn=document.querySelector('#menu-btn');
+const container=document.querySelector('.container')
+
+const aside=document.querySelector('aside')
+
+menuBtn.addEventListener('click',()=>{
+  aside.classList.toggle('open')
+  container.classList.toggle('opengrid')
+})
+
+
+const close=document.querySelector('.close');
+const open=document.querySelector('.open')
+
+close.addEventListener('click',()=>{
+  if(aside.classList.contains('open')){
+    aside.classList.remove('open')
+  }
 })
 
 
